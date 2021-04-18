781ea2fe097341b930a5abd9869e47c9ca6c99c7
snlndod <snlndod@gmail.com>
2021-04-18 18:54:11 +0800

    refactor lru cache

--- algorithms/lru_cache.py
+++ algorithms/lru_cache.py
@@ -1,17 +1,19 @@
 from collections import OrderedDict
 
-class LRUCache:
-    def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.size = capacity
 
-    def get(self, key: int) -> int:
+class LRUCache:
+    def __init__(self, k):
+        self.cache = OrderedDict()
+        self.size = k
+
+    def get(self, key):
         if key not in self.cache:
             return -1
+        # update cache
         self.cache.move_to_end(key)
         return self.cache[key]
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key, value):
         if key in self.cache:
             del self.cache[key]
         self.cache[key] = value
