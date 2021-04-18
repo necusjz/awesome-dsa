9ba90d98e4e0dc733b19e139782383d612fc5993
snlndod <snlndod@gmail.com>
2021-04-18 21:28:21 +0800

    add trie

---
+++ algorithms/trie.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.is_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.is_word = True
+
+    def search(self, word):
+        node = self.root
+        for w in word:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return node.is_word
+
+    def startswith(self, prefix):
+        node = self.root
+        for w in prefix:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return True
