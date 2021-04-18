2e9d0f18998707998844d9237887e445fad0ce63
snlndod <snlndod@gmail.com>
2021-04-18 19:24:35 +0800

    add abstract factory

---
+++ design_patterns/abstract_factory.cc
@@ -0,0 +1,87 @@
+/**
+ * Each distinct product of a product family should have a base interface. All
+ * variants of the product must implement this interface.
+ */
+class AbstractProductA {
+public:
+    virtual ~AbstractProductA() {}
+    virtual void Operation() const = 0;
+};
+/**
+ * Concrete Products are created by corresponding Concrete Factories.
+ */
+class ConcreteProductA1 : public AbstractProductA {
+public:
+    void Operation() {
+        cout << "ConcreteProductA1" << endl;
+    }
+};
+class ConcreteProductA2 : public AbstractProductA {
+public:
+    void Operation() {
+        cout << "CConcreteProductA2" << endl;
+    }
+};
+/**
+ * Here's the the base interface of another product. All products can interact
+ * with each other, but proper interaction is possible only between products of
+ * the same concrete variant.
+ */
+class AbstractProductB {
+public:
+    virtual ~AbstractProductB() {}
+    virtual void Operation() const = 0;
+};
+/**
+ * Concrete Products are created by corresponding Concrete Factories.
+ */
+class ConcreteProductB1 : public AbstractProductB {
+public:
+    void Operation() {
+        cout << "ConcreteProductB1" << endl;
+    }
+};
+class ConcreteProductB2 : public AbstractProductB {
+public:
+    void Operation() {
+        cout << "ConcreteProductB2" << endl;
+    }
+};
+/**
+ * The Abstract Factory interface declares a set of methods that return
+ * different abstract products. These products are called a family and are
+ * related by a high-level theme or concept. Products of one family are usually
+ * able to collaborate among themselves. A family of products may have several
+ * variants, but the products of one variant are incompatible with products of
+ * another.
+ */
+class AbstractFactory {
+public:
+    virtual AbstractProductA* CreateProductA() const = 0;
+    virtual AbstractProductB* CreateProductB() const = 0;
+    virtual ~AbstractFactory(){}
+};
+/**
+ * Concrete Factories produce a family of products that belong to a single
+ * variant. The factory guarantees that resulting products are compatible. Note
+ * that signatures of the Concrete Factory's methods return an abstract product,
+ * while inside the method a concrete product is instantiated.
+ */
+class ConcreteFactory1 : public AbstractFactory {
+public:
+    ConcreteProductA1* CreateProductA() {
+        return new ConcreteProductA1();
+    }
+    ConcreteProductB1* CreateProductB() {
+        return new ConcreteProductB1();
+    }
+};
+class ConcreteFactory2 : public AbstractFactory {
+public:
+    ConcreteProductA2* CreateProductA() {
+        return new ConcreteProductA2();
+    }
+    ConcreteProductB2* CreateProductB() {
+        return new ConcreteProductB2();
+    }
+};
