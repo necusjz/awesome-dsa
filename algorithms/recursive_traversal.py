f905ffa2d9d24a7e5965c3556640ed4470c043b9
umarellyh <umarellyh@gmail.com>
2021-04-23 14:10:58 +0800

    refactor recursive traversal

--- algorithms/recursive_traversal.py
+++ algorithms/recursive_traversal.py
@@ -6,38 +6,29 @@ class TreeNode:
 
 
 class Traversal:
-    @staticmethod
-    def pre_order(root):
-        ret = []
-        def dfs(node):
-            if not node:
-                return
-            ret.append(node.val)
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
-        return ret
+    def __init__(self):
+        self.ret = []
 
-    @staticmethod
-    def in_order(root):
-        ret = []
-        def dfs(node):
-            if not node:
-                return
-            dfs(node.left)
-            ret.append(node.val)
-            dfs(node.right)
-        dfs(root)
-        return ret
+    def pre_order(self, root):
+        if not root:
+            return root
+        self.ret.append(root.val)
+        self.pre_order(root.left)
+        self.pre_order(root.right)
+        return self.ret
 
-    @staticmethod
-    def post_order(root):
-        ret = []
-        def dfs(node):
-            if not node:
-                return
-            dfs(node.left)
-            dfs(node.right)
-            ret.append(node.val)
-        dfs(root)
-        return ret
+    def in_order(self, root):
+        if not root:
+            return root
+        self.in_order(root.left)
+        self.ret.append(root.val)
+        self.in_order(root.right)
+        return self.ret
+
+    def post_order(self, root):
+        if not root:
+            return root
+        self.post_order(root.left)
+        self.post_order(root.right)
+        self.ret.append(root.val)
+        return self.ret
