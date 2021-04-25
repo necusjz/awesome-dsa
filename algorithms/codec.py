aa39fc9385d6c463ba2be2bc5f98dde4364e4643
necusjz <necusjz@gmail.com>
2021-04-25 22:12:00 +0800

    add serialize and deserialize

---
+++ algorithms/codec.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Codec:
+    @staticmethod
+    def serialize(root):
+        if not root:
+            return []
+        data = []
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                data.append(node.val)
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                data.append("#")
+        return data
+
+    @staticmethod
+    def deserialize(data):
+        if not data:
+            return None
+        root = TreeNode(data[0])
+        queue = deque([root])
+        idx = 1
+        while queue:
+            node = queue.popleft()
+            if data[idx] != "#":
+                node.left = TreeNode(data[idx])
+                queue.append(node.left)
+            idx += 1
+            if data[idx] != "#":
+                node.right = TreeNode(data[idx])
+                queue.append(node.right)
+            idx += 1
+        return root
