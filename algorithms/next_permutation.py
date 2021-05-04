208496ffb17a36f3ba0c9a8df86c908adbf36cc3
umarellyh <umarellyh@gmail.com>
2021-05-05 00:09:46 +0800

    refactor next permutation

--- algorithms/next_permutation.py
+++ algorithms/next_permutation.py
@@ -3,15 +3,15 @@ class NextPermutation:
         self.nums = nums
 
     def next(self):
-        p1 = p2 = len(self.nums) - 1
-        while p1 > 0 and self.nums[p1-1] >= self.nums[p1]:
-            p1 -= 1
-        if p1 == 0:
+        l = r = len(self.nums) - 1
+        while l > 0 and self.nums[l-1] >= self.nums[l]:
+            l -= 1
+        if l == 0:
             # if it doesn't exist, return smallest
             self.nums.reverse()
         else:
-            while self.nums[p2] <= self.nums[p1-1]:
-                p2 -= 1
+            while self.nums[r] <= self.nums[l-1]:
+                r -= 1
             # swap and reverse suffix elements
-            self.nums[p1-1], self.nums[p2] = self.nums[p2], self.nums[p1-1]
-            self.nums[p1:] = self.nums[p1:][::-1]
+            self.nums[l-1], self.nums[r] = self.nums[r], self.nums[l-1]
+            self.nums[l:] = self.nums[l:][::-1]
