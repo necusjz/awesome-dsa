b76366735673ce4abb3e6fb3038a8a35895a8852
umarellyh <umarellyh@gmail.com>
2021-05-16 23:02:30 +0800

    remove unnecessary __init__

--- algorithms/sieve_of_eratosthenes.py
+++ algorithms/sieve_of_eratosthenes.py
@@ -1,20 +1,18 @@
 class SieveOfEratosthenes:
-    def __init__(self, n):
-        self.n = n
-
-    def sieve(self):
+    @staticmethod
+    def sieve(n):
         # initialize boolean array
-        is_prime = [True for _ in range(self.n + 1)]
+        is_prime = [True for _ in range(n + 1)]
         p = 2
-        while p * p <= self.n:
+        while p * p <= n:
             if is_prime[p]:
                 # update all multiples of p
-                for i in range(p * p, self.n + 1, p):
+                for i in range(p * p, n + 1, p):
                     is_prime[i] = False
             p += 1
         ret = []
         # obtain all prime numbers
-        for p in range(2, self.n + 1):
+        for p in range(2, n + 1):
             if is_prime[p]:
                 ret.append(p)
         return ret
