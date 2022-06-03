d450e13a8dd0f95fb9aa786110195efd27cb4a0d
necusjz <necusjz@gmail.com>
2022-06-04 02:30:14 +0800

    refine union_find

--- algorithms/union_find.py
+++ algorithms/union_find.py
@@ -1,14 +1,12 @@
 class UnionFind:
     def __init__(self, n):
         self.id = list(range(n))
+
+    def union(self, u, v):
+        self.id[self.find(u)] = self.find(v)
     
     def find(self, x):
         # path compression
         if self.id[x] != x:
             self.id[x] = self.find(self.id[x])
         return self.id[x]
-    
-    def union(self, u, v):
-        l = self.find(u)
-        r = self.find(v)
-        self.id[l] = r
