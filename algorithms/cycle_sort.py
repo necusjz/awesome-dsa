5f8a838b247a0d9085206871dae7150b33a15173
necusjz <necusjz@gmail.com>
2022-12-02 02:59:12 +0800

    refactoring

--- algorithms/cycle_sort.py
+++ algorithms/cycle_sort.py
@@ -1,9 +1,9 @@
 def min_swap(nums):
-    ret = 0
     n = len(nums)
     visited = [False] * n
     inverted_idx = {num: idx for idx, num in enumerate(sorted(nums))}
 
+    ret = 0
     for i in range(n):
         count = -1
         while not visited[i] and i != inverted_idx[nums[i]]:
