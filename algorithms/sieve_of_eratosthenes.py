c64c1437c138546555436052b16396019fe3a855
necusjz <necusjz@gmail.com>
2023-03-16 23:55:15 +0800

    update sieve_of_eratosthenes

--- algorithms/sieve_of_eratosthenes.py
+++ algorithms/sieve_of_eratosthenes.py
@@ -1,15 +1,16 @@
 def sieve_of_eratosthenes(n):
-    # initialize boolean array
-    is_prime = [True] * (n + 1)
+    is_prime = [True] * (n + 1)  # initialize boolean array
+
     p = 2
     while p * p <= n:
         if is_prime[p]:
             # update all multiples of p
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
+            for idx in range(p * p, n + 1, p):
+                is_prime[idx] = False
+
         p += 1
+
     ret = []
-    # obtain all prime numbers
     for p in range(2, n + 1):
         if is_prime[p]:
             ret.append(p)
