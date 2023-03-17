d1e7fc2012534e395af0993334ccc285c9952aac
necusjz <necusjz@gmail.com>
2023-03-17 23:57:49 +0800

    update trie

--- algorithms/trie.py
+++ algorithms/trie.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 
 class TrieNode:
-    """Operate on its children and determine if it's a word."""
+    """Manipulate its children and determine if it's a word."""
     def __init__(self):
         self.children = defaultdict(TrieNode)
         self.is_word = False
@@ -13,25 +13,17 @@ class Trie:
         self.root = TrieNode()
 
     def insert(self, word):
-        node = self.root
+        curr_node = self.root
         for w in word:
-            # without declaring key
-            node = node.children[w]
-        node.is_word = True
+            curr_node = curr_node.children[w]
+
+        curr_node.is_word = True
 
     def search(self, word):
-        node = self.root
+        curr_node = self.root
         for w in word:
-            node = node.children.get(w)
-            if not node:
+            curr_node = curr_node.children.get(w)
+            if not curr_node:
                 return False
-        return node.is_word
 
-    def startswith(self, prefix):
-        node = self.root
-        for w in prefix:
-            node = node.children.get(w)
-            if not node:
-                return False
-        # must be a prefix
-        return True
+        return curr_node.is_word
