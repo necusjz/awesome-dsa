82f06a8f06ab5aca6c2bd8de0393b45b8836aee7
necusjz <necusjz@gmail.com>
2023-04-03 21:03:00 +0800

    refactor heap_sort

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -1,23 +1,24 @@
 def heap_sort(nums):
     def heapify(n, i):
-        # find largest among root and children
         largest = i
-        l = 2 * i + 1
-        r = 2 * i + 2
-        if l < n and nums[i] < nums[l]:
+        l, r = i * 2 + 1, i * 2 + 2
+        # find largest among root and children
+        if l < n and nums[l] > nums[largest]:
             largest = l
-        if r < n and nums[largest] < nums[r]:
+        if r < n and nums[r] > nums[largest]:
             largest = r
+
+        # swap root and largest, continue heapify
         if largest != i:
-            # swap root and largest, continue heapify
-            nums[i], nums[largest] = nums[largest], nums[i]
+            nums[largest], nums[i] = nums[i], nums[largest]
             heapify(n, largest)
 
     n = len(nums)
-    # build max heap (n)
+    # build max heap, O(n)
     for idx in range(n // 2 - 1, -1, -1):
         heapify(n, idx)
-    # heap sort (nlogn)
+
+    # heap sort, O(nlogn)
     for idx in range(n - 1, 0, -1):
         nums[idx], nums[0] = nums[0], nums[idx]
         heapify(idx, 0)
