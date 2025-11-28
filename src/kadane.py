8941d94e902882d585be41be4b36af9bdfa1d8b4
necusjz <necusjz@gmail.com>
2025-11-28 17:19:41 +1100

    fix: kadane all negatives

--- src/kadane.py
+++ src/kadane.py
@@ -1,9 +1,7 @@
 def kadane(nums):
-    max_subarray, curr_max = 0, 0
+    max_subarray, curr_max = float("-INF"), 0
     for num in nums:
-        curr_max += num
-        curr_max = max(0, curr_max)
-
+        curr_max = max(curr_max + num, num)
         max_subarray = max(max_subarray, curr_max)
 
     return max_subarray
--- tests/test_kadane.py
+++ tests/test_kadane.py
@@ -2,6 +2,8 @@ from src.kadane import kadane
 
 
 def test_kadane():
-    nums = [-2, -3, 4, -1, -2, 1, 5, -3]
+    nums1 = [-2, -3, 4, -1, -2, 1, 5, -3]
+    nums2 = [-2, -3, -1, -4]
 
-    assert kadane(nums) == 7
+    assert kadane(nums1) == 7
+    assert kadane(nums2) == -1
